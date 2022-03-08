d72c2d3aba5668fe545f38ec5ba2bb3d4e6dacdc
Alan Czemerinski <alanczeme@gmail.com>
2022-03-08 15:15:20 -0500

    update Tile and add GamOver

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
-function Tile({tile}) {
+function Tile({tile, isSelected, handleClick}) {
 
     // let [ selectedTile, setSelectedTile ] = useState([])
 
     return (
-        <button style={{background: `${tile}`}} className="tile-button"></button>
+        <button style={{background: `${tile}`}} className={`tile-button${tile.id !== -1 ? "" : " hide-tile"}`} onClick={handleClick}></button>
     )
 }
 
