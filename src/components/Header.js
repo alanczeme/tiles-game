f8fce07d2be4582f9d04b83ec208d17c5e1a5ff9
Alan Czemerinski <alanczeme@gmail.com>
2022-03-08 15:17:20 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-function Header() {
-    return (
-        <div>Header</div>
+function Header({ restartGame }) {
+    return ( 
+        <div className="grid-header-container">
+            <div className="justify-left timer"></div>
+            <div className="justify-center game-status-text"></div>
+            <div className="justify-end">
+                <button onClick={restartGame} className="restart-button">Restart Game</button>
+            </div>
+        </div>
     )
 }
 
 export default Header;
+
+
+
