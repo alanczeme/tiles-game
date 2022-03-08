af3243f0226a07f12e49ffc88961df7fa3a16725
Alan Czemerinski <alanczeme@gmail.com>
2022-03-08 17:30:14 -0500

    fix re-starting app bug

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,20 +3,22 @@ import Tile from './Tile.js';
 
 function Board() {
     
-    let [isClicked, setIsClicked] = useState(-1);
-  
-    // Must have an even number of columns
-    const number_of_columns = 6 
-    const number_of_tiles = number_of_columns ** 2
+    const [isClicked, setIsClicked] = useState(-1);
+    const [colors, setColors] = useState([])
 
-    let colors = [];
-
-    for (let i = 0; i < number_of_tiles/2; i++) {
-        const randomColor = Math.floor(Math.random()*16777215).toString(16);
-        const randomColorHex = "#" + randomColor;
-        colors.push(randomColorHex);
-        colors.push(randomColorHex);
-    }
+    useEffect(() => {
+        let array = []
+        // Must have an even number of columns
+        const number_of_columns = 6 
+        const number_of_tiles = number_of_columns ** 2
+        for (let i = 0; i < number_of_tiles/2; i++) {
+            const randomColor = Math.floor(Math.random()*16777215).toString(16);
+            const randomColorHex = "#" + randomColor;
+            array.push(randomColorHex);
+            array.push(randomColorHex);
+        }
+        setColors(shuffle(array))
+    }, [])
 
     function shuffle(array) {
         let currentIndex = array.length,  randomIndex;
@@ -30,37 +32,10 @@ function Board() {
         }
         return array;
       }
-
-    colors = shuffle(colors);
-    // console.log(colors)
     
     function handleClick(event) {
         event.preventDefault();
         setIsClicked(event.target.id)
-
-        // const cardId = event.target.id;
-        // const newFlipps = this.state.isSelected.slice();
-        // this.setState({
-        //     prevSelectedCard: this.state.shuffledCard[cardId],
-        //     prevCardId: cardId
-        // });
-
-        // if (newFlipps[cardId] === false) {
-        //   newFlipps[cardId] = !newFlipps[cardId];
-        //   this.setState(prevState => ({ 
-        //     isSelected: newFlipps,
-        //     clickCount: this.state.clickCount + 1
-        //   }));
-
-        //   if (this.state.clickCount === 2) {
-        //     this.setState({ clickCount: 1 });
-        //     const prevCardId = this.state.prevCardId;
-        //     const newCard = this.state.shuffledCard[cardId];
-        //     const previousCard = this.state.prevSelectedCard;
-
-        //     isCardMatch(previousCard, newCard, prevCardId, cardId);
-        //   }
-        // }
     };
 
     return (
